61e298c690f9266bda81076b257270bd8167e35c
yuyang <yuyangasd@gmail.com>
2019-07-18 18:56:55 +0800

    添加等待时间 Action

---
+++ Assets/GameTransition/GTWait4SecondAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameTransition {
+	[Serializable, CreateAssetMenu( fileName = "Wait Time Action", menuName = "GT/Wait Time Action", order = 1 )]
+	public class GTWait4SecondAction : IGTAction {
+		public float duration;
+		private float time;
+
+		public override void Update() {
+			time += Time.deltaTime;
+		}
+
+		public override void OnEnable() {
+			time = 0;
+		}
+
+		public override bool Finished {
+			get {
+				return time >= duration;
+			}
+		}
+
+		public override string ToString() {
+			return string.Format( "Wait for {0} s", duration );
+		}
+	}
+}
